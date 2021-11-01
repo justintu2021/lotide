7ccf09629bdce588c61455a340cb598caf02df1e
Justin Tu <coppolavina@gmail.com>
2021-11-01 22:47:51 +0000

    use Mocha and Chai to replace test code in headTest

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,28 @@
 const head = require('../head')
 const assertEqual = require('../assertEqual');
 
+const assert = require('chai').assert;
 
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 5);
-assertEqual(head([1]), 1);
-assertEqual(head([]), 5);
-assertEqual(head([]), []);
+describe("#head", () => {
+  it("return 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1,2,3]),1)
+  });
+
+  it("return '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("return \"Hello\" for [\"Hello\", \"Lighthouse\", \"Labs\"] ", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+  
+  it("return 1 for [1]", () => {
+    assert.strictEqual(head([1]), 5);
+  });
+
+  it("return []] for []", () => {
+    assert.strictEqual(head([],[]));
+  });
+
+});
