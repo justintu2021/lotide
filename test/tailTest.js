3d4272779013b64aada6ae1c5991d9173b02830c
Justin Tu <coppolavina@gmail.com>
2021-11-02 00:20:55 +0000

    refactor tailTest, using Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,7 @@
 const tail = require('../tail')
 const assertEqual = require('../assertEqual');
 
-
+const assert = require('chai').assert;
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
@@ -10,3 +10,14 @@ assertEqual(words.length, 3);
 const words1 = ["a"];
 console.log(tail(words1));
 assertEqual(tail(words1), 1);
+
+describe('#TailTest', () => {
+  it("return Labs from [\"Yo Yo\", \"Lighthouse\", \"Labs\"]", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]),["Lighthouse", "Labs"]);
+  });
+
+  it("return Labs from [\"a\"]", () => {
+    assert.deepEqual(tail(["a"]),[]);
+  });
+});
+
